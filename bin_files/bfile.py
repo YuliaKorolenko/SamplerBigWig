bce0826059e3752484200237540034bb1dc2dcba
yul.korolenko <yulkorolenko@yandex-team.ru>
2023-12-02 16:07:58 +0300

    add bin files

---
+++ bin_files/bfile.py
@@ -0,0 +1,57 @@
+import numpy as np
+import time
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+
+PREPROCESS_FILE = "bin_files/DNA_bfile_4"
+PREPROCESS_METADATA = "bin_files/bfile_metadata_4.csv"
+WINDOW_SIZE = 10
+LOADED_BIN = np.load(PREPROCESS_FILE)
+df = pd.read_csv(PREPROCESS_METADATA)
+
+
+def read_from_bin(start : int):
+    return LOADED_BIN[start:(start + WINDOW_SIZE)]
+
+
+def get_lines(chr : int, start : int):
+    # assert start + WINDOW_SIZE  <= df.loc[chr].start + df.loc[chr].lenght, 'Make the starting position smaller or choose a different chromosome'
+    return read_from_bin(df.loc[chr].start + start)
+
+
+def get_time():
+    global WINDOW_SIZE 
+    times = []
+    sizes = []
+    for i in range(1, 10):
+        WINDOW_SIZE = 1000 * i
+
+        # print(WINDOW_SIZE)
+        # print(NORM_WINDOW_SIZE)
+        start_time = time.time()
+        for _ in range(0, 10000):
+            start_pos = random.randint(0, 20000000)
+            ans = get_lines(0, start_pos)
+        
+        res_time = time.time() - start_time
+        times.append(res_time)
+        sizes.append(WINDOW_SIZE)
+        print("--- %s seconds ---" % res_time)
+    plt.xlabel('Time in seconds')
+    plt.ylabel('Window size')    
+    plt.plot(times, sizes,'ro')
+    plt.show()
+
+if __name__ == '__main__':    
+    get_time()
+    # print(get_lines(0, 0))
+
+
+
+
+        
+    
+
+    
+    
---
+++ bin_files/preprocess_bfile.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+from tqdm import tqdm
+import pandas as pd
+import math
+import numpy as np
+
+# 000 - T
+# 001 - C
+# 010 - A
+# 011 - G
+# 100 - N
+
+PREPROCESS_FILE = "bin_files/DNA_bfile_4"
+PREPROCESS_METADATA = "bin_files/bfile_metadata_4.csv"
+
+@dataclass
+class Chromosome_Info:
+    number: int
+    start_pos: int
+    lenght: int
+    wit_sep_lenght : int
+    row_length : int
+    with_sep_row_length : int
+
+def get_positions():
+    file_fai = open('Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai')
+    start_chr_positions = [None] * 24
+    for line in file_fai:
+
+        splitted = line.split()
+        chr_num = splitted[0][3:]
+        if chr_num == 'X':
+            chr_num = 23
+        elif chr_num == 'Y':
+            chr_num = 24
+
+        chr_num = int(chr_num)
+        lenght = int(splitted[1])
+        row_length = int(splitted[3])
+        with_sep_row_length = int(splitted[4])
+        wit_sep_lenght = lenght + math.ceil(lenght / row_length) * (with_sep_row_length - row_length)
+        start_chr_positions[chr_num-1]=Chromosome_Info(chr_num, int(splitted[2]), lenght, wit_sep_lenght, row_length, with_sep_row_length)
+
+    return start_chr_positions
+
+def to_one_hot(lines : str, cur_size : int):
+    j = 0
+    G = [0] * cur_size
+    T = [0] * cur_size
+    A = [0] * cur_size
+    C = [0] * cur_size
+    for i in lines:
+        if (i == 'G'):
+            G[j] = 1
+        elif (i == 'T'):
+            T[j] = 1
+        elif (i == 'A'):
+            A[j] = 1
+        elif (i == 'C'):
+            C[j] = 1
+        j += 1
+    return G, T, A, C
+
+def preprocess_dna():
+    df = pd.DataFrame(columns=['start', 'lenght'])
+    file_fa = open('Homo_sapiens.GRCh38.dna.primary_assembly.fa')
+    answer_file = open(PREPROCESS_FILE, 'wb')
+    start_chr_positions = get_positions()
+     
+    ans_G = []
+    ans_T = []
+    ans_A = []
+    ans_C = []
+
+    j = 0
+    for i in tqdm(range(0, 24)):
+        file_fa.seek(start_chr_positions[i].start_pos)
+        line = file_fa.read(start_chr_positions[i].wit_sep_lenght - 1)
+
+        G, T, A, C = to_one_hot(line, start_chr_positions[i].wit_sep_lenght - 1)
+        ans_G += G
+        ans_T += T
+        ans_A += A
+        ans_C += C
+
+        df.loc[i] = [j, start_chr_positions[i].wit_sep_lenght - 1]
+        j += start_chr_positions[i].wit_sep_lenght - 1
+        df.to_csv(PREPROCESS_METADATA, index=False)
+
+    print("before save")    
+    np.save(answer_file, [ans_G, ans_T, ans_A, ans_C])
+    print("after save")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    preprocess_dna()
